ce3e6e36946c16aa07ba7a2f3cda23b3c8da3ed8
Khoa Ngo The <khoa@ii.inc>
2025-06-25 15:56:41 +0700

    feat: update template and system prompt

--- .templates/nextjs-shadcn/jest.setup.js
+++ .templates/nextjs-shadcn/jest.setup.js
@@ -9,20 +9,38 @@
 global.TextEncoder = require('util').TextEncoder;
 global.TextDecoder = require('util').TextDecoder;
 
+// Add ReadableStream polyfill (required for undici)
+if (typeof global.ReadableStream === 'undefined') {
+  const { ReadableStream } = require('stream/web');
+  global.ReadableStream = ReadableStream;
+}
+
+// Add WritableStream polyfill (often needed alongside ReadableStream)
+if (typeof global.WritableStream === 'undefined') {
+  const { WritableStream } = require('stream/web');
+  global.WritableStream = WritableStream;
+}
+
+// Add TransformStream polyfill
+if (typeof global.TransformStream === 'undefined') {
+  const { TransformStream } = require('stream/web');
+  global.TransformStream = TransformStream;
+}
+
 // Add polyfill for Request API
 if (typeof global.Request === 'undefined') {
-    const { Request } = require('undici');
-    global.Request = Request;
-  }
-  
-  // Add polyfill for Response API (often needed alongside Request)
-  if (typeof global.Response === 'undefined') {
-    const { Response } = require('undici');
-    global.Response = Response;
-  }
-  
-  // Add polyfill for fetch API (if not already available)
-  if (typeof global.fetch === 'undefined') {
-    const { fetch } = require('undici');
-    global.fetch = fetch;
-  }
+  const { Request } = require('undici');
+  global.Request = Request;
+}
+
+// Add polyfill for Response API (often needed alongside Request)
+if (typeof global.Response === 'undefined') {
+  const { Response } = require('undici');
+  global.Response = Response;
+}
+
+// Add polyfill for fetch API (if not already available)
+if (typeof global.fetch === 'undefined') {
+  const { fetch } = require('undici');
+  global.fetch = fetch;
+}
