5fbfb6c415c71777dd40258524fd76ef03f7fed8
Quy Pham <quy@ii.inc>
2025-06-29 12:58:27 +0700

    feat: add code-server

--- docker/nginx/docker-entrypoint.sh
+++ docker/nginx/docker-entrypoint.sh
@@ -5,7 +5,7 @@ echo "Substituting environment variables in template"
 echo "PUBLIC_DOMAIN: ${PUBLIC_DOMAIN}"
 export ESCAPED_PUBLIC_DOMAIN=$(echo "${PUBLIC_DOMAIN}" | sed 's/\./\\./g')
 echo "ESCAPED_PUBLIC_DOMAIN: ${ESCAPED_PUBLIC_DOMAIN}"
-envsubst '${PUBLIC_DOMAIN} ${ESCAPED_PUBLIC_DOMAIN} ' </etc/nginx/conf.d/default.conf.template >/etc/nginx/conf.d/default.conf
+envsubst '${PUBLIC_DOMAIN} ${ESCAPED_PUBLIC_DOMAIN} ${EXTERNAL_PORT} ' </etc/nginx/conf.d/default.conf.template >/etc/nginx/conf.d/default.conf
 
 # Show the generated config for debugging
 echo "Generated nginx config:"
